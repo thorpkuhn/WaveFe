5c5d184aebc2e9d48ee78ea28e2dfdd544e5a842
Cryptopragmatic <tristan.alberio@gmail.com>
2022-09-29 14:32:54 +0800

    pub init

---
+++ hardhat.config.js
@@ -0,0 +1,9 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@developerdao/hardhat-fe");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  fe: {
+    version: "0.19.1-alpha",
+  },
+};
